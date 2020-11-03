71c0ae4a5d33fbe621960866808baf3f5fed2dff
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-11-02 18:42:07 -0600

    fixing sql

--- rare.sql
+++ rare.sql
@@ -15,12 +15,12 @@ CREATE TABLE `Posts` (
   `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
   `user_id` INTEGER NOT NULL,
   `title` VARCHAR(30) NOT NULL,
-  `content` VARCHAR(MAX) NOT NULL,
+  `content` TEXT NOT NULL,
   `category_id` INTEGER NOT NULL,
   `publication_date` DATE NOT NULL,
-  `image_url` VARCHAR(MAX),
-  FOREIGN KEY `user_id` REFERENCES `Users`(`id`),
-  FOREIGN KEY `category_id` REFERENCES `Categories`(`id`)
+  `image_url` TEXT,
+  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
+  FOREIGN KEY (`category_id`) REFERENCES `Categories`(`id`)
 );
 
 CREATE TABLE `Tags` (
@@ -41,7 +41,7 @@ CREATE TABLE `Comments` (
   `user_id` INTEGER NOT NULL,
   `post_id` INTEGER NOT NULL,
   `subject`  VARCHAR(30) NOT NULL,
-  `content` VARCHAR(MAX) NOT NULL,
+  `content` TEXT NOT NULL,
   `creation_date` DATE NOT NULL,
   FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
   FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`)
@@ -56,8 +56,8 @@ INSERT INTO `Categories` VALUES (null, 'Technology');
 INSERT INTO `Categories` VALUES (null, 'Music');
 INSERT INTO `Categories` VALUES (null, 'Pets');
 
-INSERT INTO `Posts` VALUES (null, 2, 'The 5 Best Cat Breeds', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sapien in mi congue varius. Vestibulum nec eros nec ante tempus porttitor. Nullam sit amet justo ligula. Nam eu tellus vel nisl tempus varius.', 4, 10/23/2020, 'https://www.animalclinicofwoodruff.com/files/cat-breeds1059943207.jpg');
-INSERT INTO `Posts` VALUES (null, 1, 'How Covid Has Impacted the NFL', 'Duis eu libero enim. Donec gravida aliquam arcu, vitae viverra diam tincidunt a. Etiam luctus quis nisi ac malesuada.', 1, 11/01/2020, 'https://static.www.nfl.com/image/private/t_editorial_landscape_3_4_desktop_2x/f_auto/league/urdgxtnk14li98mcjcqv.jpg');
+INSERT INTO `Posts` VALUES (null, 2, 'The 5 Best Cat Breeds', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer bibendum sapien in mi congue varius. Vestibulum nec eros nec ante tempus porttitor. Nullam sit amet justo ligula. Nam eu tellus vel nisl tempus varius.', 4, '10/23/2020', 'https://www.animalclinicofwoodruff.com/files/cat-breeds1059943207.jpg');
+INSERT INTO `Posts` VALUES (null, 1, 'How Covid Has Impacted the NFL', 'Duis eu libero enim. Donec gravida aliquam arcu, vitae viverra diam tincidunt a. Etiam luctus quis nisi ac malesuada.', 1, '11/01/2020', 'https://static.www.nfl.com/image/private/t_editorial_landscape_3_4_desktop_2x/f_auto/league/urdgxtnk14li98mcjcqv.jpg');
 
 INSERT INTO `Tags` VALUES (null, 'Travel');
 INSERT INTO `Tags` VALUES (null, 'Relationships');
@@ -68,5 +68,7 @@ INSERT INTO `Tags` VALUES (null, 'Python');
 INSERT INTO `Post_Tags` VALUES (null, 1, 3);
 INSERT INTO `Post_Tags` VALUES (null, 2, 4);
 
-INSERT INTO `Comments` VALUES (null, 3, 2, 'Football', 'Great article, thanks!', 11/01/2020)
-INSERT INTO `Comments` VALUES (null, 1, 1, 'Cats', "I've owned three of the breeds you wrote about, and I agree that they make great pets!", 10/25/2020)
+INSERT INTO `Comments` VALUES (null, 3, 2, 'Football', 'Great article, thanks!', 11/01/2020);
+INSERT INTO `Comments` VALUES (null, 1, 1, 'Cats', "I've owned three of the breeds you wrote about, and I agree that they make great pets!", 10/25/2020);
+
+SELECT * FROM Posts
