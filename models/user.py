28970f56c2785e2bec4c9e793c8aea29b577715f
Kamiran Ibrahim <kamiran.ibrahim@gmail.com>
2020-11-02 21:35:45 -0600

    users

---
+++ models/user.py
@@ -0,0 +1,9 @@
+class User():
+
+    def __init__(self, id, first_name, last_name, display_name, email = "", password = ""):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.display_name = display_name
+        self.email = email
+        self.password = password
