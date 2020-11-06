9472e2fedc7f0f61450512c7503561e3f6ec149d
Ryan Beiden <rdbeiden@gmail.com>
2020-11-05 18:47:09 -0600

    wip

--- request_handler.py
+++ request_handler.py
@@ -80,9 +80,9 @@ class HandleRequests(BaseHTTPRequestHandler):
         elif resource == 'login':
             new_object = get_users_by_email(post_body['username'], post_body['password'])
             #new_object['valid'] = True
-        elif resource == 'post':
+        elif resource == 'posts':
             new_object = create_post(post_body)
-        elif resource == 'tag':
+        elif resource == 'tags':
             new_object = create_tag(post_body)
 
         self.wfile.write(f"{new_object}".encode())
